e52f693941ee49ceebaba15d3c7ad4bfa90a9623
HP <samir13030313@gmail.com>
2025-11-24 20:49:53 +0500

    Задание 1. Повторите число

---
+++ homework2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main() {
+  int number; 
+
+  std::cout << "Vvedite chislo: " << std::endl; 
+  std::cin >> number; 
+
+  std::cout << "Vi veli: " << number << std::endl; 
+
+  return 0; 
+}
